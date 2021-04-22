e873de37970ea30374b026c7106895fbee7210cd
Vaibhav <vaibhsin@adobe.com>
2021-04-22 18:35:20 +0530

    Chnages in code

---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=20bd456330f237b24b1891145f6da7da&query=${latitude},${longitude}`;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        'Outside it is ' +
+          body.current.weather_descriptions[0] +
+          '. ' +
+          ' It is currently ' +
+          body.current.temperature +
+          ' degress out. There is a ' +
+          body.current.precip +
+          '% chance of rain.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
